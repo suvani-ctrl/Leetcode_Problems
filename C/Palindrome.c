02f354d2ecb3cbc49ddb0554c365610d018f1d32
suvani-ctrl <dd3064073@gmail.com>
2025-03-13 10:00:57 +0545

    Added new solutions

--- C/Palindrome.c
+++ C/Palindrome.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 
 bool isPalindrome(int x) {
+    int original = x;
+     if(x<0) return false;
     int reverse = 0;
+   
     while (x > 0)
     {
             int digit = (x%10);
-            reverse = reverse * 0 + digit;
+            reverse = reverse * 10 + digit;
             x/=10;
     }
-        return reverse;
+        return reverse == original;
 }
